a1a5a5f397e6e710bb100f2706f31e369f131121
zhuguangxx <78968658+zhuguangxx@users.noreply.github.com>
2026-04-12 17:13:35 +0800

    Add supabase.js

---
+++ supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Missing SUPABASE_URL or SUPABASE_SERVICE_KEY');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

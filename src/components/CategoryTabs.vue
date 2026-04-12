df4edb54f82ae477089d2b88b22cc40d659053bf
zhuguangxx <78968658+zhuguangxx@users.noreply.github.com>
2026-04-12 17:13:12 +0800

    Add src/components/CategoryTabs.vue

---
+++ src/components/CategoryTabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="category-tabs">
+    <van-tabs v-model:active="activeTab" @change="onChange" shrink>
+      <van-tab title="木门" name="木门" />
+      <van-tab title="玻璃门" name="玻璃门" />
+      <van-tab title="窗户" name="窗户" />
+    </van-tabs>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+  modelValue: { type: String, default: '木门' }
+});
+
+const emit = defineEmits(['update:modelValue', 'change']);
+const activeTab = ref(props.modelValue);
+
+watch(() => props.modelValue, (val) => {
+  activeTab.value = val;
+});
+
+function onChange(name) {
+  emit('update:modelValue', name);
+  emit('change', name);
+}
+</script>
+
+<style scoped>
+.category-tabs {
+  background: #fff;
+}
+</style>

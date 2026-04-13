f3c5a9302d04ffa29e0204d71a585e05c194d640
zhuguangxx <78968658+zhuguangxx@users.noreply.github.com>
2026-04-13 15:53:18 +0800

    Update public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'door-showcase-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
